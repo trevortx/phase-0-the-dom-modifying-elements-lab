dd8e176e259f0e2af03e7ee5d22e7f62bc903a79
Trevor Vardeman <trevortx@gmail.com>
2022-01-17 15:40:18 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Trevor is the champion";
+document.body.append(newHeader);
